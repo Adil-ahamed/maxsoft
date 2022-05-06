390c07a5cad5d30a1f92d3cf7e424c25ef2ca00a
Adil Ahamed <85915375+Adil-ahamed@users.noreply.github.com>
2022-05-06 16:27:39 +0530

    Add files via upload

---
+++ maxsoft.js
@@ -0,0 +1,80 @@
+function showOrHideDiv() {
+    var v = document.getElementById("image-container");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ }
+
+
+// client logo slide
+  $(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
+
+
+$(document).ready(function(){
+    $('.image-gallery').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 1
+            }
+        }]
+    });
+});
+
+$(document).ready(function(){
+    $('.mobile-image-gallery').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 1
+            }
+        }]
+    });
+});
